9e075de3a5160c6166f84d84a2f3d714e3219eb6
im-nishantg <96784121+im-nishantg@users.noreply.github.com>
2024-02-02 20:29:51 +0530

    idk

--- src/test/java/com/billhub/qa/testcases/LoginPageTest.java
+++ src/test/java/com/billhub/qa/testcases/LoginPageTest.java
@@ -15,6 +15,7 @@ public class LoginPageTest extends TestBase{
 	TaxationDashboardPage taxationDashboardPage;
 	BaDashboardPage baDashboardPage;
 	CommercialDashboardPage commercialDashboardPage;
+	
 	public LoginPageTest(){
 		super();
 	}
